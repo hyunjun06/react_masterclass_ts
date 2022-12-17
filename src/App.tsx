d13ae44f3cdf6a58e955a747cacf93a9b6c2647e
hyunjun06 <junkayden@gmail.com>
2022-12-17 22:28:12 +0900

    Typescript #3.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+    const [username, setUsername] = useState("");
+    
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+
+        setUsername(value);
+    };
+    
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        alert(username);
+        setUsername("");
+    };
+
     return (
         <div>
-            <Circle bgColor="tomato" text="random text"/>
-            <Circle bgColor="teal" borderColor="black"/>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="username" value={username} onChange={onChange} />
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
