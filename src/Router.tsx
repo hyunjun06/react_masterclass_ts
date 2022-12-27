45588555791a09b5da46899960ded1e7ff403265
hyunjun06 <junkayden@gmail.com>
2022-12-27 15:46:50 +0900

    React Router #4.4

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./screens/Home";
 import About from "./screens/About";
+import NotFound from "./screens/NotFound";
 
 const router = createBrowserRouter([
     {
@@ -10,13 +11,15 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "",
-                element: <Home />
+                element: <Home />,
+                errorElement: <NotFound />, // error when children occurs runtime error
             },
             {
                 path: "about",
                 element: <About />
             }
-        ]
+        ],
+        errorElement: <NotFound />,
     }
 ]);
 
